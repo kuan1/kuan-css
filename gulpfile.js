a26f60b8ce7d219c42bf6a60e9fe71c4bb6a67d5
luzhongkuan <luzhongk@126.com>
2020-03-02 10:30:40 +0800

    chore: postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
-const gulp = require("gulp");
-const less = require("gulp-less");
-const rename = require("gulp-rename");
-const cleanCSS = require("gulp-clean-css");
-const postcss = require("gulp-postcss");
+const gulp = require('gulp')
+const less = require('gulp-less')
+const rename = require('gulp-rename')
+const cleanCSS = require('gulp-clean-css')
+const postcss = require('gulp-postcss')
 
-const lessPath = "less/index.less";
+const lessPath = 'less/index.less'
 
 function styles() {
   return gulp
     .src(lessPath)
     .pipe(less())
-    .pipe(postcss([require("autoprefixer")]))
+    .pipe(postcss([require('autoprefixer')]))
     .pipe(cleanCSS())
-    .pipe(rename("index.css"))
-    .pipe(gulp.dest("./"));
+    .pipe(rename('index.css'))
+    .pipe(gulp.dest('./'))
 }
 
-exports.default = styles;
+exports.default = styles
